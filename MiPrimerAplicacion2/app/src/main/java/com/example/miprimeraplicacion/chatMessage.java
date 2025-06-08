bbd1809bf24e56894fe076329a34e9a9261ecb4d
gisseledepaz <ustr031124@ugb.edu.sv>
2025-06-07 21:31:33 -0600

    Continuamos con el chats de amigos

---
+++ MiPrimerAplicacion2/app/src/main/java/com/example/miprimeraplicacion/chatMessage.java
@@ -0,0 +1,11 @@
+package com.example.miprimeraplicacion;
+
+public class chatMessage {Add commentMore actions
+    public boolean posicion; //izquierdo o derecho.
+    public String mensaje;
+    public chatMessage(boolean posicion, String mensaje){
+        super();
+        this.posicion = posicion;
+        this.mensaje = mensaje;
+    }
+}
